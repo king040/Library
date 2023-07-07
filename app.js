73ecfbcc6a2e0f5e837362343eed2d2ecf8c0a5e
king040 <78076586+king040@users.noreply.github.com>
2023-07-07 08:29:30 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () =>{
+        console.log(title + " by" + " " + author + ", " + pages + ", " + read);
+    } 
+}
+
+const theWicther = new Book('The Wicther', 'G.R Martin', 2000 , 'not read yet');
+const harryPotter = new Book('Harry Potter', "j.k rollin", 1000 , "100000reader")
+const onePunchMan = new Book('One Punch Man','ONE Sensei', 4000, "50million otaku" );
+
+
+// ! Object Constructors
+theWicther.info()
+harryPotter.info()
+onePunchMan.info()
+
+
+// ! Object Prototype
+Book.prototype.wantToRead = function(){
+    console.log("I want to read " + this.title);
+}
+
+theWicther.wantToRead()
+harryPotter.hasOwnProperty('valueOf');
+
+
---
+++ library.js
@@ -0,0 +1,112 @@
+// Book class 
+
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
+// UI class
+
+class UI {
+    static displayBooks() {
+        const books = Store.getBooks();
+        books.forEach(book=> UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const tableBody = document.querySelector("#book-table-body");
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td><a href="#" class="delete">Delete</a></td>`
+            ;
+        tableBody.appendChild(row);
+    }
+
+    static deleteBook(element) {
+        if(element.classList.contains("delete")) {
+            element.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement("div");
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector("body");
+        container.insertBefore(div, document.querySelector("h1"));
+        setTimeout(() => document.querySelector(".alert").remove(), 3000);
+    }
+
+    static clearFields() {
+        document.querySelector("#title").value = "";
+        document.querySelector("#author").value = "";
+    }
+}
+
+
+// Store class
+
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem("books") === null){
+            books =[];
+        } else {
+            books = JSON.parse(localStorage.getItem("books"));
+        }
+        return books;
+    }
+
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem("books", JSON.stringify(books)); 
+    }
+
+    static removeBook(title) {
+        const books = Store.getBooks();
+        books.forEach((book, index) => {
+            if(book.tltle === title){
+                books.splice(index, 1);
+            }
+        });
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+}
+
+
+// Event: display books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+// Event: Add a book
+document.querySelector("#add-book-form").addEventListener("submit",(e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#title').value;
+
+
+    if (title === "" || author === "") {
+        UI.showAlert("please fill in all fields", "error");
+    } else {
+        const book = new Book(title, author);
+
+        UI.addBookToList(book);
+        Store.addBook(book);
+        UI.showAlert("Book added successfully", "success");
+        UI.clearFields();
+
+    }
+})
+
+
+// Event: Remove a book
+document.querySelector('#book-list').addEventListener("click",(e) => {
+    UI.deleteBook(e.target);
+    Store.removeBook(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+    UI.showAlert("Book removed successsfully", "success")
+});
